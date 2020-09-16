308df94355230b25910ecbca22a37b1a74326509
Mike Chi <mikechi@reclaimmikechi.lan>
2020-09-15 20:47:22 -0700

    importing bootstrap

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 import React from 'react';
+import Navbar from './components/navbar/Navbar';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+      <Navbar />
     </div>
   );
 }
