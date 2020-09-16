e9c6e8d73e57a31430de450fdc1107e186ced7cd
Mike Chi <mikechi@reclaimmikechi.lan>
2020-09-15 21:33:05 -0700

    test

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -5,6 +5,7 @@ const Navbar = function () {
     <section className="navbar-parent">
       <div className="nav-left">
         <div className="logo">MC</div>
+        <div className="logo">MC2</div>
       </div>
       <div className="nav-right"></div>
     </section>
